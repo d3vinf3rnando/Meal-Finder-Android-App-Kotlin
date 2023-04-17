504fe0c44955e7e5b4131cb6fe339f9abf3e0922
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-04-17 08:05:43 +0530

    database quarries updated

--- app/src/main/java/com/example/cw2_2020021/MealDao.kt
+++ app/src/main/java/com/example/cw2_2020021/MealDao.kt
@@ -1,4 +1,20 @@
 package com.example.cw2_2020021
 
-class MealDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface MealDao {
+    //writing quarries for database
+
+    @Query("SELECT * FROM meal_table")
+    fun getAll():List<Meal>
+
+    // onConflict ignore is to ignore data duplication
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(meal: Meal)
+
 }
